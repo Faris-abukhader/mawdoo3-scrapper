38ca3493cb4cbf6d33abac0d490aa11218d418ae
Faris-abukhader <70070951+Faris-abukhader@users.noreply.github.com>
2022-12-31 20:16:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 from mawdoo3 import Mawdoo3
 mawdoo3 = Mawdoo3()
-mawdoo3.save_all_articles_into_file()
+# first this function
+mawdoo3.save_all_articles_title_into_file()
+# then run this function and pass the name of generated file from upon function
+# mawdoo3.save_all_articles_into_file()
