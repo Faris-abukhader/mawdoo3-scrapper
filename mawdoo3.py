29b1e7178935beda7cf618d265a3cc97ea012574
Faris-abukhader <70070951+Faris-abukhader@users.noreply.github.com>
2022-12-31 20:14:19 +0800

    updated selenium click button issue

--- mawdoo3.py
+++ mawdoo3.py
@@ -7,48 +7,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import time
-
+import json
 
 class Mawdoo3():
 
-    def __init__(self):
+    def __init__(self,chunk_length=2000):
         self.url = 'https://mawdoo3.com/'
         self.website = get(self.url).content
         self.content = BeautifulSoup(self.website, 'html.parser')
         self.categories = self.content.find_all("div", {"class": "category"})
         self.scrapped_categories = []
-        self.scrapped_articles = []
-
-    def proccess_data(self, data):
-        arabic_alpha_unicodes = ['\u0627', '\u0628', '\u0622', '\u062a', '\u062b', '\u062c', '\u062d', '\u062e',
-                                 '\u062f', '\u0630',
-                                 '\u0631', '\u0632', '\u0633', '\u0634', '\u0635', '\u0636', '\u0637', '\u0638',
-                                 '\u0639',
-                                 '\u063a', '\u0641', '\u0642', '\u0643', '\u06a9', '\u0644', '\u0645', '\u0646',
-                                 '\u0648',
-                                 '\u0629', '\u0647', '\u064a', '\u0621', '\u0649', '\u0623', '\u0624', '\u0625',
-                                 '\u0626']
-        data = re.sub(' +', ' ', data)  # to remove any multi spaces
-        data = araby.strip_tashkeel(data)  # to remove tashkeel
-        patterns_regex = '(' + '|'.join(arabic_alpha_unicodes) + ')'
-        data = re.sub(f'[^{patterns_regex}^\s]', '',
-                      data)  # to remove any character that is not in arabic alphabetic system
-        data = re.sub('(آ|إ|أ)', 'ا', data)  # normalize hamzah
-        data = re.sub('\u0629', 'ه', data)  # sub tah with hah
-        for alph in arabic_alpha_unicodes:  # remove duplicate letter more than 2
-            data = re.sub(f'({alph})' + '{3,}', alph, data)
-        data = '\n'.join([re.sub(' +', ' ', line) for line in data.split('\n') if line != ''])
-        return data.strip()
-
-    def get_categories(self):
-        for category in self.categories:
-            for category_item in category.find_all('li'):
-                if not category_item.find('a')['href'].startswith('http'):
-                    self.scrapped_categories.append(category_item.find('a')['href'])
-
-    def get_category_articles(self, title):
-        url = f'https://mawdoo3.com{title}'
-        print(f'Category url : {url}')
+        self.scrapped_articles = {}
+        self.articles = []
+        self.get_categories()
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36"
         options = webdriver.ChromeOptions()
         options.headless = True
@@ -63,36 +34,92 @@ class Mawdoo3():
         options.add_argument('--disable-gpu')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--no-sandbox')
-        driver = webdriver.Chrome(options=options)
-        driver.get(url)
+        self.driver = webdriver.Chrome(options=options)
+        self.chunk_length = chunk_length
 
-        while True:
-            try:
-                see_more_btn = WebDriverWait(self.driver, 10).until(
-                    EC.presence_of_element_located((By.ID, 'more_results_btn')))
-                see_more_btn.click()
-                time.sleep(2)
-            except:
-                break
-        website = driver.page_source
+    def get_categories(self):
+        for category in self.categories:
+            main_category = category.find_next("h2").find("span", {"class": "title"}).text
+            category_dic = {"main_category": main_category, "sub_category": [sub_category["href"] for sub_category in
+                                                                             category.find_next("ul").find_all("a")]}
+            self.scrapped_categories.append(category_dic)
+
+
+    def get_category_articles(self, main_category, category):
+        url = f'https://mawdoo3.com{category}'
+        print(f'{url} under scrapping . . . ')
+        self.driver.get(url)
+        click_more = True
+        try:
+            element = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.ID, 'more_results_btn')))
+            counter = 1
+            while click_more:
+                time.sleep(10)
+                print(f'{url} see more articles . . . ({counter})')
+                if element.is_displayed():
+                    # element.click()
+                    self.driver.execute_script('arguments[0].click()', element)
+                else:
+                    click_more = False
+                counter += 1
+        except Exception as e:
+            print(f'{url} Error . . . {e}')
+
+        website = self.driver.page_source
         content = BeautifulSoup(website, 'html.parser')
         articles = content.find_all("a", {"class": "category-box"})
-        self.scrapped_articles.extend([article['href'] for article in articles])
-        driver.close()
+        if main_category in self.scrapped_articles:
+            self.scrapped_articles[main_category].extend([article['href'] for article in articles])
+        else:
+            self.scrapped_articles[main_category] = [article['href'] for article in articles]
+        # self.driver.close()
 
-    def save_all_articles_title_into_file(self, file_name=f'{(int(time.time() * 1000))}_mawdoo3_article_titles.txt'):
-        self.get_categories()
-        for category in self.scrapped_categories:
-            self.get_category_articles(category)
-        articles_file = open(file_name, 'a')
-        articles_file.write(',\n'.join(self.scrapped_articles))
-        articles_file.close()
+    def save_all_articles_title_into_file(self, file_name=f'{(int(time.time() * 1000))}_mawdoo3_article_titles.json'):
+        for index, category in enumerate(self.scrapped_categories):
+            main_category = category['main_category']
+            for sub_category in category['sub_category']:
+                print(f'scrapping main category : {main_category} -> sub category : {sub_category}')
+                self.get_category_articles(main_category, sub_category)
+        print(len(self.scrapped_articles))
 
-    def get_target_article(self, title):
+        with open(file_name, 'w') as file:
+            json.dump(self.scrapped_articles, file)
+
+    def extract_sub_category(self, strCateg=''):
+        slash_matches = [i for i, ltr in enumerate(strCateg) if ltr == '/']
+        result = ''
+        try:
+           result = strCateg[slash_matches[0]:slash_matches[1]].strip()
+        except:
+           pass
+        if result.find(',') != -1:
+            result = result.split(',')[1].strip()
+        if result.find('،') != -1:
+            result = result.split('،')[1].strip()
+        if result.find('/') != -1:
+            result = result.split('/')[1].strip()
+        return result
+
+    def get_target_article(self, category, title):
         url = f'https://mawdoo3.com{title}'
         website = get(url).content
         content = BeautifulSoup(website, 'html.parser')
-        article = content.find('div', {'class': 'article-text'})
+        sub_category = ''
+        article = ''
+
+        try:
+          article = content.find('div', {'class': 'article-text'})
+        except:
+          pass
+        try :
+          sub_category = content.find('ul', {'class': 'breadcrumbs'}).text
+          sub_category = self.extract_sub_category(sub_category)
+        except:
+          pass
+        try:
+            [script.decompose() for script in article.find_all('script')]
+        except:
+            pass
         try:
             article.find('div', {'class': 'printfooter'}).decompose()
         except:
@@ -121,32 +148,34 @@ class Mawdoo3():
             article.find('div', {'id': 'feedback-thanks-msg'}).decompose()
         except:
             pass
-        article_category = content.find_all('span', {'itemprop': 'name'})
-        if len(article_category) < 2:
-            print('Error no category found')
-            return
-        article_text = self.proccess_data(data=article.text)
-        self.scrapped_articles.append(f'{article_category[1].text}|{article_text}')
+        try:
+          article = article.text
+        except:
+            pass
 
+        article = {'category': category, 'sub_category': sub_category, 'title': title, 'content': article}
+        # print(article)
+        self.articles.append(article)
 
-    def save_articles_into_file(self, file_name=f'{(int(time.time() * 1000))}_mawdoo3_articles.txt'):
-        articles_title = open('mawdoo3_all_article_titles.txt').read()
-        articles_title_list = articles_title.split(',')
-        for index, title in enumerate(articles_title_list):
-            print(f'{index + 1}. {title.strip()}')
-            self.get_target_article(title.strip())
-        articles_file = open(file_name, 'a')
-        articles_file.write(',\n'.join(self.scrapped_articles))
-        articles_file.close()
-
-    def save_all_articles_into_file(self, file_name=f'{(int(time.time() * 1000))}_mawdoo3_articles.txt'):
-        self.get_categories()
-        for index, category in enumerate(self.scrapped_categories):
-            print(f'Reading category number : {index + 1} of {len(self.scrapped_categories)}')
-            for index, article in enumerate(self.get_category_articles(category)):
-                print(f'Reading article number : {index + 1}')
-                self.scrapped_articles.append(f'{category}|{article}|{self.get_target_article(article)}')
-        print(f'Total articles read is : {len(self.scrapped_articles)}')
-        file = open(file_name, 'w')
-        file.write(',\n'.join(self.scrapped_articles))
-        file.close()
+    def save_articles_into_file(self, titles_file):
+        with open(titles_file) as json_file:
+            data = json.load(json_file)
+            counter = 1
+            category_counter = 1
+            for category, title_list in data.items():
+                print(f'{category_counter}. {category} is under scrapping . . . ')
+                for title in title_list:
+                    print(f'{counter}. {self.url}{title} scrapping article . . . ')
+                    self.get_target_article(category, title)
+                    counter += 1
+                    if counter % self.chunk_length == 0 :
+                      file_name=f'{(int(time.time() * 1000))}_mawdoo3_articles.json'
+                      with open(file_name, 'w') as json_file:
+                         json.dump(self.articles, json_file, ensure_ascii=False)
+                         print(f'Save file : {file_name}')
+                         self.articles = []
+                category_counter += 1
+            file_name=f'{(int(time.time() * 1000))}_mawdoo3_articles.json'
+            with open(file_name, 'w') as json_file:
+                  json.dump(self.articles, json_file, ensure_ascii=False)
+                  print(f'Save file : {file_name}')
