660080fe4093aea137639a76d747c47590f0a797
Faris-abukhader <70070951+Faris-abukhader@users.noreply.github.com>
2023-01-13 14:45:10 +0800

    updating calling async methond

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ mawdoo3 = Mawdoo3()
 # first this function
 mawdoo3.save_all_articles_title_into_file()
 # then run this function and pass the name of generated file from upon function
-# mawdoo3.save_all_articles_into_file()
+# asyncio.run(mawdoo3.save_articles_into_file('file_name_here.json'))
